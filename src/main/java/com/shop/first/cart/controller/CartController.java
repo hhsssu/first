e126ccad1528653d59dc9a096ea942f042c5c478
abc20210708 <gotqlc2670@naver.com>
2022-06-05 16:12:00 +0900

    장바구니 (2)

--- src/main/java/com/shop/first/cart/controller/CartController.java
+++ src/main/java/com/shop/first/cart/controller/CartController.java
@@ -1,4 +1,108 @@
 package com.shop.first.cart.controller;
 
+import com.shop.first.cart.domain.Cart;
+import com.shop.first.cart.service.CartService;
+import com.shop.first.customer.domain.Customer;
+import com.shop.first.product.domain.Product;
+import com.sun.deploy.net.HttpResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/cart")
+@Log4j2
+@RequiredArgsConstructor
 public class CartController {
-}
+
+    private final CartService cartService;
+
+    //장바구니 추가
+    @PostMapping("/add")
+    public String insert(Cart cart, HttpSession session, HttpServletResponse response)
+        throws IOException, ServletException {
+
+        log.info("장바구니 insert! " +session.getAttribute("loginCustomer"));
+        Customer loginCustomer = (Customer) session.getAttribute("loginCustomer");
+        int count = cartService.countCart(loginCustomer.getCsId(), cart.getPrCode());
+
+        log.info("(cart):" +cart);
+        log.info("로그 확인하기 1: "+ loginCustomer.getCsId());
+        log.info("로그 확인하기 2: "+ cart.getPrCode());
+
+        //장바구니에 기존 상품이 있는지 검사
+        log.info("count =============> "+ count);
+        if (count == 0)  {
+            log.info("장바구니 상품 레코드 확인 Controller");
+            cart.setCsId(loginCustomer.getCsId());
+            cartService.insert(cart);
+            log.info(cart);
+        }
+        else {
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("text/html; charset=UTF-8");
+
+            PrintWriter out = response.getWriter();
+
+            out.println("<script>alert('이미 장바구니에 있는 상품입니다 :) ');");
+            out.println("history.back()");
+            out.println("</script>");
+            out.flush();
+            response.flushBuffer();
+            out.close();
+            return null;
+        }
+
+        return "redirect:/cart/list";
+
+    }
+
+    //장바구니 목록
+    @GetMapping("/list")
+    public String list(HttpSession session, Model model) {
+
+        log.info("장바구니 목록 Controller! (화면)");
+
+        Customer loginCustomer = (Customer) session.getAttribute("loginCustomer");
+        //장바구니 정보
+        List<Cart> cartList = cartService.listCart(loginCustomer.getCsId());
+
+        log.info(cartList);
+        model.addAttribute("cart",cartList);
+
+        List<Product> productList = new ArrayList<>();
+
+        //상품정보
+        for (Cart cart: cartList) {
+            Product product = cartService.listProduct(cart.getPrCode());
+            productList.add(product);
+            model.addAttribute("product",productList);
+        }
+
+        //장바구니 전체 금액 호출
+        //int sumTotal = cartService.sumTotal(csId);
+
+        // return "redirect:/cart/list?csId="+loginCustomer.getCsId();
+        return "cart/list";
+    }
+
+    //장바구니 삭제
+    @GetMapping("/delete")
+    public String delete(int cartCode) {
+        log.info("장바구니 삭제: " + cartCode);
+        cartService.delete(cartCode);
+        return "redirect:/cart/list";
+    }
+}//
--- src/main/java/com/shop/first/cart/service/CartService.java
+++ src/main/java/com/shop/first/cart/service/CartService.java
@@ -1,4 +1,64 @@
 package com.shop.first.cart.service;
 
+import com.shop.first.cart.domain.Cart;
+import com.shop.first.cart.dto.ModCart;
+import com.shop.first.cart.repository.CartMapper;
+import com.shop.first.product.domain.Product;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
 public class CartService {
-}
+
+    private final CartMapper cartMapper;
+
+    //장바구니 추가
+    public void insert(Cart cart) {
+        cartMapper.insert(cart);
+        log.info("장바구니 추가 "+ cart);
+    }
+
+    //장바구니 목록
+    public List<Cart> listCart(String csId) {
+        log.info("장바구니 목록 요청 서비스!" + csId);
+        return cartMapper.listCart(csId);
+    }
+
+    //장바구니 상품 목록
+    public Product listProduct(int prCode) {
+        log.info("CartService 상품 목록 요청 서비스!" + prCode);
+        return cartMapper.listProduct(prCode);
+    }
+
+    //장바구니 삭제
+    public void delete(int cardCode) {
+        cartMapper.delete(cardCode);
+    }
+
+    //장바구니 수정
+    public void modifyCart(ModCart cart) {
+        cartMapper.modifyCart(cart);
+    }
+
+    //장바구니 금액 합계
+    public int sumTotal(String csId) {
+        return cartMapper.sumTotal(csId);
+    }
+
+    //장바구니 동일한 상품 레코드 확인
+    public int countCart(String csId, int prCode){
+        log.info("장바구니 상품 레코드 확인 Service =====>"+ cartMapper.countCart(csId, prCode));
+        return cartMapper.countCart(csId, prCode);
+    }
+
+    //장바구니 상품 수량 변경
+    public void updateCart(Cart cart) {
+        cartMapper.updateCart(cart);
+    }
+
+}//
