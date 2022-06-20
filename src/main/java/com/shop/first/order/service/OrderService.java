0524b240bdf253ece6346018bda73c6e3fa87f2b
abc20210708 <gotqlc2670@naver.com>
2022-06-20 19:54:23 +0900

    Create OrderService.java

---
+++ src/main/java/com/shop/first/order/service/OrderService.java
@@ -0,0 +1,44 @@
+package com.shop.first.order.service;
+
+import com.shop.first.cart.repository.CartMapper;
+import com.shop.first.customer.repository.CustomerMapper;
+import com.shop.first.product.repository.ProductMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final CustomerMapper customerMapper;
+
+    private final CartMapper cartMapper;
+
+    private final ProductMapper productMapper;
+    //주문
+    /*
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count){
+
+        //엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        //주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        //주문 저장
+        orderRepository.save(order);
+
+        return order.getId();
+
+    } */
+
+}//
