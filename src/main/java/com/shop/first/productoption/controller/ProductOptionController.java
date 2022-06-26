e1e41daa1fcf8c1a03ce35c35ac9182fc126eb13
abc20210708 <gotqlc2670@naver.com>
2022-06-26 22:28:37 +0900

    OptionController 수정 (1)

---
+++ src/main/java/com/shop/first/productoption/controller/ProductOptionController.java
@@ -0,0 +1,16 @@
+package com.shop.first.productoption.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequestMapping("/option")
+@RequiredArgsConstructor
+public class ProductOptionController {
+
+
+
+}//
---
+++ src/main/java/com/shop/first/productoption/repository/ProductOptionMapper.java
@@ -0,0 +1,10 @@
+package com.shop.first.productoption.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProductOptionMapper {
+
+
+
+}//
