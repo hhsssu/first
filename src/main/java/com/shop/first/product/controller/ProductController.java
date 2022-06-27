f84ec03134580ac69effe4a47b53f816479abbf0
abc20210708 <gotqlc2670@naver.com>
2022-06-27 16:29:53 +0900

    Create product_insert.jsp

--- src/main/java/com/shop/first/product/controller/ProductController.java
+++ src/main/java/com/shop/first/product/controller/ProductController.java
@@ -7,10 +7,17 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 @Controller
@@ -38,7 +45,72 @@ public class ProductController extends HttpServlet {
     }
 
 
+    //상품 등록 화면 요청(화면)
+    @GetMapping("/write")
+    public String write() {
+        log.info("/product/write - GET!");
 
+        /*Product product = (Product) session.getAttribute("newProduct");
+        if (product == null) {
+            return "redirect:/home";
+        }*/
+        return "product/product_insert";
+    }
+
+    //상품 등록 요청 - POST!
+    @PostMapping("/write")
+    public String fileUpload(MultipartHttpServletRequest request, Product product,
+                             @RequestParam("files") MultipartFile[] files) throws Exception  {
+
+        log.info("상픔 등록 요청 - POST!");
+
+
+        //String uploadPath = request.getSession().getServletContext().getRealPath("/"); webapp
+
+        String uploadPath = "C:\\testImg";
+
+        String fileOriginName = "";
+
+        for (int i = 0; i < files.length; i++) {
+            fileOriginName = files[i].getOriginalFilename();
+            log.info("기존 파일명: "+ fileOriginName);
+            SimpleDateFormat formatter = new SimpleDateFormat("YYYYMMDD_HHMMSS_"+i);
+            Calendar now = Calendar.getInstance();
+
+            String fileMultiName = "";
+            //확장자명
+            String extension = fileOriginName.split("\\.")[1];
+
+            //
+            fileOriginName = formatter.format(now.getTime())+"."+extension;
+            log.info("변경된 파일명 :" +fileOriginName);
+
+            File f = new File(uploadPath+ "\\"+fileOriginName);
+            files[i].transferTo(f);
+
+
+            fileMultiName += fileOriginName;
+            if (i==0) {
+                product.setPrThumb(fileMultiName);
+            } else if (i == 1 ) {
+                product.setPrImg1(fileMultiName);
+            } else if (i == 2 ) {
+                product.setPrImg2(fileMultiName);
+            } else if (i == 3 ) {
+                product.setPrImg3(fileMultiName);
+            }  else if (i == 4 ) {
+                product.setPrImg4(fileMultiName);
+            }  else if (i == 5 ) {
+                product.setPrImg5(fileMultiName);
+            }
+            else {fileMultiName += ","+fileOriginName;}
+            log.info("*"+fileMultiName);
+        }
+
+        productService.write(product);
+        log.info(product);
+        return "redirect:/main/index";
+    }
 
 
 
---
+++ src/main/webapp/WEB-INF/views/product/product_insert.jsp
@@ -0,0 +1,136 @@
+<%@ page contentType="text/html; charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<!DOCTYPE html>
+<html lang="ko">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>상품 등록</title>
+
+  </head>
+
+<body>
+
+  
+    <h1>파일 업로드</h1>
+   
+   <form action="/product/write" method="post" autocomplete="off" enctype="multipart/form-data">
+      
+    <p>
+        <label>
+            # 상품명  : <input type="text" name="prName">
+        </label>
+    </p>
+        
+
+    <p>
+        <label>
+            # 카테고리 번호  : <input type="text" name="cateCode">
+           
+            <br>
+            1 - tableware, 2 - 프라이팬, 3 - 컵, 4 - 접시
+
+        </label>
+        
+    </p>
+
+
+    <p>   
+        <label>
+            # 상품 가격  : <input type="text" name="prPrice"  >
+        </label>
+       
+    </p> 
+
+    <p>
+        <label>
+            # 판매여부  : <input type="text" name="prYn"  >
+        </label>
+       
+    </p>
+
+    <p>
+        <label>
+            # 상품수량 : <input type="text" name="prAmount" >
+        </label>
+        
+    </p>
+
+    <p>
+        <label>
+            # 상품썸네일 : <input type="file" name="files" multiple="multiple">
+           
+        </label>
+       
+    </p>
+
+
+    <p>
+        <label>
+            # 상품이미지1 : <input type="file" name="files" multiple="multiple" >
+           
+        </label>
+       
+    </p>
+
+
+    <p>
+        <label>
+            # 상품이미지2 : <input type="file" name="files" multiple="multiple" >
+           
+        </label>
+       
+    </p>
+
+    <p>
+        <label>
+            # 상품이미지3 : <input type="file" name="files" multiple="multiple"  >
+           
+        </label>
+       
+    </p>
+
+    <p>
+        <label>
+            # 상품이미지4: <input type="file" name="files" multiple="multiple"  >
+           
+        </label>
+       
+    </p>
+
+    <p>
+        <label>
+            # 상품이미지5 : <input type="file" name="files" multiple="multiple"  >
+           
+        </label>
+       
+    </p>
+
+
+    <c:forEach var="o" items="${option}"> 
+    <p>
+        <label>
+          <input type="checkbox" value="o" > <!-- onclick="location.href='list.jsp' -->
+        </label>
+    </p>
+    </c:forEach>
+  
+
+    <p>
+        <label>
+          <input type="submit" value="등록하기" > <!-- onclick="location.href='list.jsp' -->
+        </label>
+    </p>
+
+    
+    </form>
+
+    
+
+
+</body>
+</html>
