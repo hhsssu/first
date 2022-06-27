60ef05359e344b05e002dcb0ea720c9d037de1bb
abc20210708 <gotqlc2670@naver.com>
2022-06-27 16:50:03 +0900

    Update ProductOptionService.java

--- src/main/java/com/shop/first/productoption/service/ProductOptionService.java
+++ src/main/java/com/shop/first/productoption/service/ProductOptionService.java
@@ -1,12 +1,25 @@
 package com.shop.first.productoption.service;
 
+import com.shop.first.productoption.domain.ProductOption;
+import com.shop.first.productoption.repository.ProductOptionMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Log4j2
 @RequiredArgsConstructor
 public class ProductOptionService {
+
+    private final ProductOptionMapper productOptionMapper;
+
+    //옵션 목록 중간 처리
+    public List<ProductOption> getList() {
+
+        return productOptionMapper.getArticles();
+    }
+
 
 }//
